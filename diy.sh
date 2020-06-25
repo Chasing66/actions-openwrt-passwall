3621132e5a1f0f73a49e58b7fa13c19ab5ea7477
Chasing66 <32839114+Chasing66@users.noreply.github.com>
2020-06-25 12:27:53 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,10 +5,10 @@
 # https://github.com/kenzok8
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/litte.git package/litte
 date=`date +%m.%d.%Y`
-sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D by kenzo'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D by Chasing'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 cp -f default-settings package/*/*/default-settings/files/zzz-default-settings
 ./scripts/feeds update -a
